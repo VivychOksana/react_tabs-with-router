e34421e3a7cc78d0400347e542f5855b8330c844
Vivych Oksana <vivichokcana@gmail.com>
2019-12-26 22:48:26 +0200

    Updates

---
+++ precache-manifest.b9bdbf1304f6ec42df1c5ab24faa1061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6403f6e77caa5bae2aeb7865790e9758",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "cc9710aaddb1c9d71742",
+    "url": "/react_tabs-with-router/static/css/main.f6cc11ef.chunk.css"
+  },
+  {
+    "revision": "c6d38b044a4284916b35",
+    "url": "/react_tabs-with-router/static/js/2.0ba52e99.chunk.js"
+  },
+  {
+    "revision": "cc9710aaddb1c9d71742",
+    "url": "/react_tabs-with-router/static/js/main.2db8f426.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
